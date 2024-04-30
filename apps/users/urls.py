d298cb0e9722eb1fc308f7afa242fb15d88a9105
Iri O <oparina.iri@gmail.com>
2024-04-30 19:58:28 +0500

    Added urls for app

---
+++ apps/users/urls.py
@@ -0,0 +1,56 @@
+from django.urls import path
+
+from users.views import (
+    RegisterView,
+    LoginView,
+    LogoutView,
+    EmailConfirmView,
+    SettingsView,
+    PasswordResetRequestView,
+    PasswordResetView,
+    ProfileView,
+)
+
+urlpatterns = [
+    path(
+        'register/',
+        RegisterView.as_view(),
+        name='register',
+    ),
+    path(
+        'login/',
+        LoginView.as_view(),
+        name='login',
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(),
+        name='logout',
+    ),
+    path(
+        'confirm_email/<str:url_hash>/',
+        EmailConfirmView.as_view(),
+        name='confirm_email',
+    ),
+    path(
+        'settings/',
+        SettingsView.as_view(),
+        name='settings',
+    ),
+    path(
+        'password_reset/request/',
+        PasswordResetRequestView.as_view(),
+        name='password_reset_request',
+    ),
+    path(
+        'password_reset/<str:url_hash>/',
+        PasswordResetView.as_view(),
+        name='password_reset',
+    ),
+    path(
+        'profile/<int:pk>',
+        ProfileView.as_view(),
+        name='profile',
+    )
+]
+
