a0f4a4e8e8daf23e901b5d634deee5a4a2949c77
Iri O <oparina.iri@gmail.com>
2024-05-02 19:40:16 +0500

    Added index app urls

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('post/', include('posts.urls')),
+    path('', include('index.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
