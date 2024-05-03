62ceb7252f33b2db6f31628bc5b3c5beeedf36a0
Iri O <oparina.iri@gmail.com>
2024-05-03 19:19:30 +0500

    Changed email field in list display

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,7 +5,7 @@ from users.models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = (
-        'email',
+        'username',
         'is_staff',
         'email_confirmed',
     )
