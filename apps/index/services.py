65a433796a3d571655793d0350188447bb728b67
Iri O <oparina.iri@gmail.com>
2024-05-04 19:56:07 +0500

    Changed get_posts

--- apps/index/services.py
+++ apps/index/services.py
@@ -1,9 +1,11 @@
+from django.db.models import Q
+
 from posts.models import Post
 
 
-def get_posts():
+def get_posts(request):
     try:
-        posts = Post.objects.all()
+        posts = Post.objects.filter(~Q(hide_from_users=request.user))
         return 200, posts
     except Exception as exc:
         print(exc)
