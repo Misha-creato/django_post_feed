adc48d11963824b71c510387f2e6384db27f042c
Iri O <oparina.iri@gmail.com>
2024-05-04 19:57:43 +0500

    Added hide_from_users field

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -14,6 +14,11 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts',
     )
+    hide_from_users = models.ManyToManyField(
+        to=User,
+        blank=True,
+        related_name='hidden_posts',
+    )
     title = models.CharField(
         max_length=256,
         verbose_name='Заголовок',
@@ -36,9 +41,14 @@ class Post(models.Model):
     slug = models.SlugField(
         unique=True,
         verbose_name='URL',
+        blank=True,
     )
 
     def save(self, *args, **kwargs):
+        if not self.image:
+            if self.pk:
+                post = Post.objects.get(pk=self.pk)
+                self.image = post.image
         slug_title = slugify(self.title)
         hash_id = hashlib.sha256(str(self.id).encode()).hexdigest()[:12]
         self.slug = f'{slug_title}#{hash_id}'
@@ -49,6 +59,7 @@ class Post(models.Model):
         return self.title
 
     class Meta:
+        ordering = ['-created_at']
         db_table = 'posts'
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
