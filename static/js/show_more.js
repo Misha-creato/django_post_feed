acf51a78c5940eb158139fa93f2c30cded7f3db0
Iri O <oparina.iri@gmail.com>
2024-05-14 06:30:06 +0500

    Revert "commit"
    This reverts commit d3bdabb60621ebc3a256723a89c778fe6e0eba56.

--- static/js/show_more.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('showMoreButton').addEventListener('click', function() {
-    document.getElementById('moreText').style.display = 'block';
-    this.style.display = 'none';
-});
